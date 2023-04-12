8e15ed211cb94fe922ba2dab8d367684720b895b
Ashish Raj <ashi1110000111@gmail.com>
2023-04-12 22:21:02 +0530

    Update firebase.js

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -2,12 +2,7 @@ import { initializeApp } from "firebase/app";
 import {getFirestore, collection} from 'firebase/firestore'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB3V04tb6mW1SNlT69BDtp5391ZQnqr168",
-  authDomain: "filmyverse-b1dec.firebaseapp.com",
-  projectId: "filmyverse-b1dec",
-  storageBucket: "filmyverse-b1dec.appspot.com",
-  messagingSenderId: "1093987459964",
-  appId: "1:1093987459964:web:37522752a58b6943ea3af9"
+ 
 };
 
 const app = initializeApp(firebaseConfig);
